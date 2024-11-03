2408708018a236e9df01633182bc4fe1b48a6043
sbx_user1051 <sbx_user1051@169.254.80.245>
2024-11-03 14:28:06 +0000

    Sun Nov 03 14:28:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,7 +11,7 @@ def checkout(skus):
         'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 
         'I': 35, 'J': 60, 'K': 70, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 
         'Q': 30, 'R': 50, 'S': 20, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 17, 
-        'Y': 20, 'Z': 21
+        'Y': 20, 'Z': 20
     }
     item_counts = {sku: 0 for sku in valid_skus}
     total_price = 0
@@ -91,6 +91,7 @@ def apply_special_offer(item_counts, prices):
             items_used_in_sets -= 1
 
     return total_price
+
 
 
 
