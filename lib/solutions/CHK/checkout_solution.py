e7453318ea7a2041479b273b413213aee50bd586
sbx_user1051 <sbx_user1051@169.254.80.245>
2024-11-03 12:36:42 +0000

    Sun Nov 03 12:36:42 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,25 +5,19 @@
 def checkout(skus):
     valid_skus = ['A', 'B', 'C', 'D', 'E']
     total_price = 0
-    total_A = 0
-    total_B = 0
+    item_counts = {sku: 0 for sku in valid_skus}
 
     if not isinstance(skus, str) or not all(char in valid_skus for char in skus):
         return -1
 
     for sku in skus:
-        if sku == 'A':
-            total_A += 1
-        elif sku == 'B':
-            total_B += 1
-        total_price += get_price(sku)
+        item_counts[sku] += 1
 
-        if total_A == 3:
-            total_price -= 20
-            total_A = 0
-        if total_B == 2:
-            total_price -= 15
-            total_B = 0
+    total_price -= (item_counts['A'] // 3) * 20
+    total_price -= (item_counts['B'] // 2) * 15
+    free_Bs = item_counts['E'] // 2
+    total_price -= min(free_Bs, item_counts['B']) * prices['B']
+    
     return total_price
 
 def get_price(sku):
@@ -37,3 +31,4 @@ def get_price(sku):
         return 15
     elif(sku == 'E'):
         return 40
+
